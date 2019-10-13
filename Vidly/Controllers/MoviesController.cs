cc548fea8a03443fc957ede8d18c2d51a1d90b3d
Nathan Pena <nnpena@outlook.com>
2019-10-12 20:35:49 -0500

    Second commit

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
+using Microsoft.SqlServer.Server;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() {Name = "Shrek!"};
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer One"},
+                new Customer {Name = "Customer Two"}
+            };
+            
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "NAME";
+
+            return Content($"pageIndex={pageIndex}&sortBy={sortBy}");
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByRelaseDate(int year, int month)
+        {
+            return Content($"year={year}&month={month}");
+        }
+
+    }
+}
