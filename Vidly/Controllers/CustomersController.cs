07c058b3a959bba9bd22f2671603e7f60c203b80
Nathan Pena <nnpena@outlook.com>
2019-10-12 21:12:43 -0500

    Add table to the customers index

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer {Id = 1, Name = "John Pena"},
+                new Customer {Id = 2, Name = "Greg Solis"}
+            };
+
+            var viewModel = new IndexCustomerViewModel {Customers = customers};
+            return View(viewModel);
+        }
+    }
+}
