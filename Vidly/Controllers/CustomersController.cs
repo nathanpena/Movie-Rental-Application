bbca05c51b383db465c9827b1438b603faf88d90
Nathan Pena <nnpena@outlook.com>
2019-10-13 19:52:37 -0500

    Update CustomersCtrl to get db data

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -11,28 +11,39 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
-       
-       public ActionResult Index()
-        {
-            var customers = GetCustomers();
 
-            var viewModel = new IndexCustomerViewModel {Customers = customers};
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        public ActionResult Index()
+        {
+            
+            var viewModel = new IndexCustomerViewModel { Customers = _context.Customers.ToList() };
             return View(viewModel);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
-            {                
+            {
                 return HttpNotFound();
             }
             else
             {
                 return View(customer);
             }
-            
+
         }
 
         private IEnumerable<Customer> GetCustomers()
