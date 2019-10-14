564529096b0abf0c693000c1d0ef3ea94b2c8000
Nathan Pena <nnpena@outlook.com>
2019-10-13 19:42:58 -0500

    Made updates to customers model and db

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -11,7 +11,7 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
-       //a test
+       
        public ActionResult Index()
         {
             var customers = GetCustomers();
