cead2f5b0f533bd95939af156d29e31238d1d1f1
Nathan Pena <nnpena@outlook.com>
2019-10-15 20:10:19 -0500

    Add New Movie form

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -9,12 +9,21 @@ namespace Vidly.Models
     public class Movie
     {
         public int Id { get; set; }
+        
         public string Name { get; set; }
+        
         [Required]
         public Genre Genre { get; set; }
+        
+        [Display(Name = "Genre")]
         public byte GenreId { get; set; }
+        
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
+        
         public DateTime DateAdded { get; set; }
+        
+        [Display(Name = "Number in Stock")]
         public int NumberInStock { get; set; }
     }
 }
