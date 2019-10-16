515defdc41fa23bdeef9e98ca92cbd53cd5f9a36
Nathan Pena <nnpena@outlook.com>
2019-10-15 19:04:12 -0500

    Add form to create and edit customers

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -14,7 +14,7 @@ namespace Vidly.Controllers
     {
 
         private ApplicationDbContext _context;
-
+          
         public CustomersController()
         {
             _context = new ApplicationDbContext();
@@ -23,6 +23,54 @@ namespace Vidly.Controllers
         protected override void Dispose(bool disposing)
         {
             _context.Dispose();
+        }
+
+        public ActionResult New()
+        {
+            var membershipTypes = GetMembershipTypes();
+            var viewModel = new CustomerFormViewModel
+            {
+                MembershipTypes = membershipTypes
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+ 
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Customers");
+        }
+
+        [HttpPost]
+        public ActionResult Update(Customer customer)
+        {
+
+            var DbCustomer = GetCustomer(customer.Id);
+            Map(DbCustomer, customer);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Customers");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = GetCustomer(id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            var membershipTypes = GetMembershipTypes();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                MembershipTypes = membershipTypes,
+                Customer = customer
+            };
+
+            return View(viewModel);
         }
 
         public ActionResult Index()
@@ -34,7 +82,7 @@ namespace Vidly.Controllers
 
         public ActionResult Details(int id)
         {
-            var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
+            var customer = GetCustomer(id);
 
             if (customer == null)
             {
@@ -45,6 +93,24 @@ namespace Vidly.Controllers
                 return View(customer);
             }
 
+        }
+
+        private Customer GetCustomer(int id)
+        {
+            return _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
+        }
+
+        private IEnumerable<MembershipType> GetMembershipTypes()
+        {
+            return _context.MembershipTypes.ToList();
+        }
+
+        private void Map(Customer DbCustomer, Customer customer)
+        {
+            DbCustomer.Name = customer.Name;
+            DbCustomer.Birthdate = customer.Birthdate;
+            DbCustomer.MembershipTypeId = customer.MembershipTypeId;
+            DbCustomer.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
         }
 
     }
